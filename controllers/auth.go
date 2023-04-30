b1cae55ab1cd1744de20a17b1758f15f9dac5dd6
sunny0714 <sun2020victory@gmail.com>
2023-04-29 18:57:14 -0700

    add: added input validation in register endpoint

--- controllers/auth.go
+++ controllers/auth.go
@@ -6,6 +6,18 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type RegisterInput struct {
+	Username string `json:"username" binding:"required"`
+	Password string `json:"password" binding:"required"`
+}
+
 func Register(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{"data": "hello from controller!"})
+
+	var input RegisterInput
+
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"message": "Validated!"})
 }
